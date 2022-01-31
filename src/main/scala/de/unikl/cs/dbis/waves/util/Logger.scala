3b0a026c4c4f7ce0f3aad29959b5401895ef8e57
Patrick Hansert <Hansert.Patrick@googlemail.com>
2022-01-31 16:08:58 +0100

    feat: Log to HDFS

--- src/main/scala/de/unikl/cs/dbis/waves/util/Logger.scala
+++ src/main/scala/de/unikl/cs/dbis/waves/util/Logger.scala
@@ -1,9 +1,15 @@
 package de.unikl.cs.dbis.waves.util
 
+import java.nio.charset.StandardCharsets
 import java.util.Calendar
 import scala.collection.mutable.ListBuffer
 
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.conf.Configuration
+import java.text.SimpleDateFormat
+
 object Logger {
+    val logDir = "log"
 
     protected case class Event(name: String, time: Long, payload: String) {
         override def toString() = s"$time,\"$name\",\"$payload\""
@@ -11,14 +17,31 @@ object Logger {
 
     private val events = ListBuffer.empty[Event]
 
+    private def now = Calendar.getInstance().getTime()
+    private val dateFormat = new SimpleDateFormat("yyyy-mm-dd-HH-mm-ss")
+
     def log(name : String, payload: Any = "") = {
-        val now = Calendar.getInstance()
-        val timestamp = now.getTime().getTime()
-        events.addOne(Event(name, timestamp, payload.toString))
+        events.addOne(Event(name, now.getTime(), payload.toString))
     }
 
-    def printToStdout() = {
-        println(events.mkString("\n"))
+    override def toString(): String = events.mkString("\n")
+
+    def clear() = events.clear()
+
+    def printToStdout() = println(this)
+
+    def writeToDisk(config : Configuration) = {
+        val path = new Path(s"$logDir/log-${dateFormat.format(now)}.csv")
+        val fs = path.getFileSystem(config)
+        val out = fs.create(path)
+        out.write(toString().getBytes(StandardCharsets.UTF_8))
+        out.close()
+    }
+
+    def flush(config : Configuration) = {
+        printToStdout()
+        writeToDisk(config)
+        clear()
     }
   
 }

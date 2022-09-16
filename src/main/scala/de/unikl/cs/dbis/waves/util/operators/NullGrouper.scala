b29e51761b86496cf62987202f470a94516b4c9b
Patrick Hansert <hansert@informatik.uni-kl.de>
2022-09-16 11:22:42 +0200

    feat: implement a Grouper for when we don't want to group

---
+++ src/main/scala/de/unikl/cs/dbis/waves/util/operators/NullGrouper.scala
@@ -0,0 +1,37 @@
+package de.unikl.cs.dbis.waves.util.operators
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.typedLit
+
+/**
+  * The NullGrouper is used in cases where no actual grouping happens.
+  * The grouped representation is identical to the data.
+  *
+  * @param GROUP_COLUMN the name of the Column containing the group information
+  */
+object NullGrouper extends Grouper {
+    
+  val MATCH_COLUMN = TempColumn("partition")
+
+  // Grouped representation is identical to the source data
+  override def group(data: DataFrame): DataFrame = data
+
+  // Since we assume all groups from bucket are present in data and our groups
+  // are just Rows, we can just return bucket
+  override def find(bucket: DataFrame, data: DataFrame) = bucket
+
+  // Since we assume all groups from bucket are present in data and our groups
+  // are just Rows, we can just return bucket
+  override def sort(bucket: DataFrame, data: DataFrame) = bucket
+
+  // All groups are individual rows
+  override def count(df: DataFrame) = df.count()
+  override def matchColumn: TempColumn = MATCH_COLUMN
+
+  // This is the only function in this grouper that actually does something
+  override def matchAll(buckets: Seq[DataFrame], data: DataFrame) = {
+    buckets.zipWithIndex
+           .map({case (b, i) => b.withColumn(matchColumn, typedLit(i))})
+           .reduce(_.union(_))
+  }
+}
---
+++ src/test/scala/de/unikl/cs/dbis/waves/util/operators/NullGrouperSpec.scala
@@ -0,0 +1,41 @@
+package de.unikl.cs.dbis.waves.util.operators
+
+import org.scalatest.Inspectors._
+import de.unikl.cs.dbis.waves.WavesSpec
+import de.unikl.cs.dbis.waves.DataFrameFixture
+
+import org.apache.spark.sql.functions.typedLit
+
+class NullGrouperSpec extends WavesSpec
+  with DataFrameFixture {
+
+  "A NullGrouper " should {
+    "do nothing when grouping" in {
+      NullGrouper(df) should equal (df)
+    }
+    "match the groups to the correct rows" in {
+      Given("two buckets")
+      val g1 = df.limit(3)
+      val g2 = df.except(g1)
+
+      When("we match them")
+      val result = NullGrouper.matchAll(Seq(g1, g2), df)
+
+      Then("the rows should be matched correctly")
+      val mc = NullGrouper.matchColumn
+      val expected = g1.withColumn(mc, typedLit(0)).union(g2.withColumn(mc, typedLit(1)))
+      result.collect should contain theSameElementsAs (expected.collect)
+    }
+    "find all data belonging to a bucket" in {
+      val g1 = df.limit(3)
+      NullGrouper.find(g1, df).collect should contain theSameElementsAs (g1.collect())
+    }
+    "order the data by the grouped data" in {
+      val g1 = df.limit(3).orderBy("b.c")
+      NullGrouper.sort(g1, df).collect should contain theSameElementsInOrderAs (g1.collect)
+    }
+    "come up with the correct count" in {
+      NullGrouper.count(df) should equal (8)
+    }
+  }
+}

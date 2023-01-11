1fa3fb2012f79a506c54ab6e763742e494bf1de5
Patrick Hansert <hansert@informatik.uni-kl.de>
2023-01-11 12:22:14 +0100

    feat: make the shape for the manual partitioning job configurable

--- src/main/scala/de/unikl/cs/dbis/waves/testjobs/split/Manual.scala
+++ src/main/scala/de/unikl/cs/dbis/waves/testjobs/split/Manual.scala
@@ -2,24 +2,30 @@ package de.unikl.cs.dbis.waves.testjobs.split
 
 import de.unikl.cs.dbis.waves.testjobs.JobConfig
 import de.unikl.cs.dbis.waves.partitions.{Bucket, SplitByPresence}
+import de.unikl.cs.dbis.waves.partitions.PartitionTreeHDFSInterface
 import de.unikl.cs.dbis.waves.split.PredefinedSplitter
+import de.unikl.cs.dbis.waves.sort.NoSorter
 
 object Manual extends SplitRunner {
   def main(args: Array[String]) : Unit = {
     val jobConfig = JobConfig.fromArgs(args)
     val spark = jobConfig.makeSparkSession("Manual Partition")
 
-    val manualShape = SplitByPresence( "quoted_status"
-                                     , Bucket("quotes")
-                                     , SplitByPresence( "retweeted_status"
-                                                      , Bucket("retweets")
-                                                      , SplitByPresence( "delete"
-                                                                       , "deletes"
-                                                                       , "normal"
-                                                                       )
-                                                      )
-                                     )
-    val splitter = new PredefinedSplitter(manualShape)
+    val defaultShape = SplitByPresence( "quoted_status"
+                                       , Bucket("quotes")
+                                       , SplitByPresence( "retweeted_status"
+                                                        , Bucket("retweets")
+                                                        , SplitByPresence( "delete"
+                                                                         , "deletes"
+                                                                         , "normal"
+                                                                         )
+                                                        )
+                                       )
+    val (manualShape, manualSorter) = jobConfig.getString("knownSchemaPath").map { path =>
+      val loadedTree = PartitionTreeHDFSInterface(spark, path).read().get
+      (loadedTree.root, loadedTree.sorter)
+    }.getOrElse((defaultShape, NoSorter))
+    val splitter = new PredefinedSplitter(manualShape).sortWith(manualSorter)
 
     runSplitter(spark, jobConfig, splitter)
   }

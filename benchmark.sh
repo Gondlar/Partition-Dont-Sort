74aca756fca0471d2fe42c575a7b8331f7b6f7e3
Patrick Hansert <hansert@informatik.uni-kl.de>
2023-11-09 12:55:13 +0100

    feat: update benchmark script for current experiments

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-for N in 1 2 .. 10
+NUMBER_OF_EXPERIMENT_RUNS=10
+NUMBER_OF_WARMUP_RUNS=5
+
+SOURCE="file:///cluster-share/benchmarks/json/twitter/109g_multiple"
+TARGET="hdfs://namenode:9000/benchmark"
+SCHEMA="./schemas/twitter.json"
+
+function submit_task {
+    /spark/bin/spark-submit --driver-memory 96g --master spark://`hostname`:7077 --executor-memory 96G \
+                            --class de.unikl.cs.dbis.waves.testjobs.$1 waves.jar \
+                            inputPath=$SOURCE wavesPath=$TARGET inputSchemaPath=$SCHEMA \
+                            useColumnSplits=false cleanWavesPath=true ${@:2}
+}
+
+# Warm up the Cache
+for N in $(seq 1 1 $NUMBER_OF_WARMUP_RUNS)
 do
-    sbt "runMain de.unikl.cs.dbis.waves.testjobs.CompleteScanNormal" --batch
-    sbt "runMain de.unikl.cs.dbis.waves.testjobs.CompleteScanWaves" --batch
-    sbt "runMain de.unikl.cs.dbis.waves.testjobs.PartialScanNormal" --batch
-    sbt "runMain de.unikl.cs.dbis.waves.testjobs.PartialScanWaves" --batch
+    submit_task StoreSchema
+done
+
+# Run experiments
+for N in $(seq 1 1 $NUMBER_OF_EXPERIMENT_RUNS)
+do
+    for partitions in $(seq 10 10 250)
+    do
+        submit_task split.ModelGini numPartitions=$partitions
+        submit_task split.ModelGiniWithSort numPartitions=$partitions
+        submit_task split.LexicographicMono numPartitions=$partitions
+        submit_task split.LexicographicPartitionwise numPartitions=$partitions
+    done
 done

418744dcf7b99f6381d17bb2e46a6b6aee1749e7
Patrick Hansert <hansert@informatik.uni-kl.de>
2023-12-04 17:16:24 +0100

    fix: dont clear files before allocation

--- benchmark_allocation.sh
+++ benchmark_allocation.sh
@@ -10,16 +10,16 @@ function submit_task {
     /spark/bin/spark-submit --driver-memory 96g --master spark://`hostname`:7077 --executor-memory 96G \
                             --class de.unikl.cs.dbis.waves.testjobs.$1 waves.jar \
                             inputPath=$SOURCE wavesPath=$TARGET inputSchemaPath=$SCHEMA \
-                            useColumnSplits=false cleanWavesPath=true ${@:2}
+                            useColumnSplits=false ${@:2}
 }
 
 # Run experiments
 
-submit_task split.ModelGini numPartitions=$partitions
-submit_task EvaluateAllocation reportName=allocation/gini.csv
-submit_task split.ModelGiniWithSort numPartitions=$partitions
-submit_task EvaluateAllocation reportName=allocation/sortedGini.csv
-submit_task split.LexicographicMono numPartitions=$partitions
-submit_task EvaluateAllocation reportName=allocation/mono.csv
-submit_task split.LexicographicPartitionwise numPartitions=$partitions
-submit_task EvaluateAllocation reportName=allocation/partitioned.csv
+submit_task split.ModelGini cleanWavesPath=true numPartitions=$partitions
+submit_task EvaluateAllocation cleanWavesPath=false reportName=allocation/gini.csv
+submit_task split.ModelGiniWithSort cleanWavesPath=true numPartitions=$partitions
+submit_task EvaluateAllocation cleanWavesPath=false reportName=allocation/sortedGini.csv
+submit_task split.LexicographicMono cleanWavesPath=true numPartitions=$partitions
+submit_task EvaluateAllocation cleanWavesPath=false reportName=allocation/mono.csv
+submit_task split.LexicographicPartitionwise cleanWavesPath=true numPartitions=$partitions
+submit_task EvaluateAllocation cleanWavesPath=false reportName=allocation/partitioned.csv
